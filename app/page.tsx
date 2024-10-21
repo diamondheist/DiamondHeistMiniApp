6e8c353f9b42a54a86ba1c5c8903a937f2b949d3
diamonheist <diamondheisttm@gmail.com>
2024-10-22 00:13:41 +0100

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,17 @@
-'use client'
+export const dynamic = "force-dynamic";
 
-import WebApp from '@twa-dev/sdk'
-import { useEffect, useState } from 'react'
+import TelegramAuth from '@/components/TelegramAuth';
+import { getSession } from '@/utils/session';
 
-// Define the interface for user data
-interface UserData {
-  id: number;
-  first_name: string;
-  last_name?: string;
-  username?: string;
-  language_code: string;
-  is_premium?: boolean;
-}
 
-export default function Home() {
-  const [userData, setUserData] = useState<UserData | null>(null)
-
-  useEffect(() => {
-    if (WebApp.initDataUnsafe.user) {
-      setUserData(WebApp.initDataUnsafe.user as UserData)
-    }
-  }, [])
+export default async function Home() {
+  const session = await getSession()
 
   return (
-    <main className="p-4">
-      {userData ? (
-        <>
-          <h1 className="text-2xl font-bold mb-4">User Data</h1>
-          <ul>
-            <li>ID: {userData.id}</li>
-            <li>First Name: {userData.first_name}</li>
-            <li>Last Name: {userData.last_name || 'N/A'}</li>
-            <li>Username: {userData.username || 'N/A'}</li>
-            <li>Language Code: {userData.language_code}</li>
-            <li>Is Premium: {userData.is_premium ? 'Yes' : 'No'}</li>
-          </ul>
-        </>
-      ) : (
-        <div>Loading...</div>
-      )}
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold mb-8">Jwt Authentication for Telegram Mini Apps</h1>
+      <pre>{JSON.stringify(session, null, 2)}</pre>
+      <TelegramAuth />
     </main>
   )
 }
